256066f2b14aab2aa35c6fd1d8e2df63a78eaab1
Gabrielle <ms.grodarte@gmail.com>
2024-05-13 13:08:46 -0700

    complete laB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm Gabrielle</h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw1la21EpZvhItEnVIOA6yuf&ust=1715715827451000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCm5NSxi4YDFQAAAAAdAAAAABAE" alt="Profile Picture"/>
+    <h2>About Me</h2>
+    <p>My biography</p>
+    <div>
+      <a href="https://github.com">GitHub</a>
+      <a href="https://linkedin.com">LinkedIn</a>
+    </div>
+  </div>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,55 @@
-import { render, screen } from "@testing-library/react";
+import { getByAltText, render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", ()=>{
+    render(<App/>)
+
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument()
+})
+test("renders an image tag with alt text", ()=>{
+    render(<App/>)
+
+    const image = screen.getByRole("img")
+
+    expect(image).toBeInTheDocument()
+    expect(image).toHaveAttribute("alt")
+})
+test("displays a second-level heading with the text `About Me`", ()=>{
+    render(<App/>)
+
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    })
+})
+test("displays a paragraph for your biography", ()=>{
+    render(<App/>)
+
+    const biography = screen.getByText(/biography/)
+
+    expect(biography).toBeInTheDocument()
+})
+test("displays link to github and linkedin", ()=>{
+    render(<App/>)
+
+    const githubLink = screen.getByRole("link", {
+        name: /github/i
+    })
+    const linkedInLink = screen.getByRole("link", {
+        name: /linkedin/i,
+
+    })
+
+    expect(githubLink).toHaveAttribute("href")
+    expect(linkedInLink).toHaveAttribute("href")
+    
+})
